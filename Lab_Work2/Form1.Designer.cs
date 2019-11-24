df3741202ccec6ed98bbaa679bac3588d0919b3b
RdrStels <kandakova0002@gmail.com>
2019-11-24 23:21:27 +0500

    Second

---
+++ Lab_Work2/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace Lab_Work2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(286, 46);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Загрузить...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(286, 297);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TabStop = false;
+            this.textBox1.WordWrap = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(309, 10);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(302, 460);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.groupBox3);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(294, 434);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Быстрая";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox2.Location = new System.Drawing.Point(5, 6);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(286, 283);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TabStop = false;
+            this.textBox2.WordWrap = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Location = new System.Drawing.Point(5, 302);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(286, 129);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Данные";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Кол - во сравнений:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Кол - во перестановок:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Время:";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Controls.Add(this.groupBox4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(294, 434);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Сортировка с подчётом";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox3.Location = new System.Drawing.Point(4, 5);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(286, 283);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.TabStop = false;
+            this.textBox3.WordWrap = false;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label4);
+            this.groupBox4.Controls.Add(this.label5);
+            this.groupBox4.Controls.Add(this.label6);
+            this.groupBox4.Location = new System.Drawing.Point(4, 301);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(286, 129);
+            this.groupBox4.TabIndex = 2;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Данные";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Кол - во сравнений:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Кол - во перестановок:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 36);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Время:";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(6, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(286, 46);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Сортировать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(5, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(298, 322);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Входной Файл:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(5, 334);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(298, 132);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Управление";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MintCream;
+            this.ClientSize = new System.Drawing.Size(614, 478);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Lab№2";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Lab_Work2/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Lab_Work2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<double> numb = new List<double>();
+        int swapping = 0; int comprassion = 0;
+        private void Cleaning()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            numb.Clear();
+            label1.Text = "Время:   ";
+            label2.Text = "Кол-во перестановок:";
+            label3.Text = "Кол-во сравнений:";
+            label6.Text = "Время:   ";
+            label5.Text = "Кол-во перестановок:";
+            label4.Text = "Кол-во сравнений:";
+
+        }
+        private void QuickSort<T>(T[] data, int left, int right) where T : IComparable<T>
+        {
+            int i, j;
+            T pivot, temp;
+            i = left;
+            j = right;
+            pivot = data[(left + right) / 2];
+
+            do
+            {
+                while ((data[i].CompareTo(pivot) < 0) && (i < right)) i++;
+                while ((pivot.CompareTo(data[j]) < 0) && (j > left)) j--;
+                if (i <= j)
+                {
+                    comprassion++;
+                    swapping++;
+                    temp = data[i];
+                    data[i] = data[j];
+                    data[j] = temp;
+                    i++;
+                    j--;
+                }
+            } while (i <= j);
+
+            if (left < j){comprassion++; QuickSort(data, left, j);}
+            if (i < right){comprassion++; QuickSort(data, i, right);}
+        }
+        public void CountingVoid(List<double> n2)
+        {
+            swapping = 0; comprassion = 0;
+            var start = DateTime.Now;
+            int[] sorted = СoutingSort(n2);
+            var spendtime = DateTime.Now - start;
+            List<string> tempr = new List<string>();
+            for (int i = 0; i < sorted.Length; i++) tempr.Add(Convert.ToString(sorted[i]));
+            string g = String.Join("\r\n", tempr);
+            textBox3.Text = g;
+            label6.Text = "Время:   " + spendtime.TotalMilliseconds +" мс";
+            label5.Text = "Кол-во перестановок:" + swapping;
+            label4.Text = "Кол-во сравнений:" + comprassion;
+        }
+        private int[] СoutingSort(List<double> array)
+        {
+            int[] sortedArray = new int[array.Count];
+            int minVal = (int)array[0];
+            int maxVal = (int)array[0];
+            for (int i = 0; i < array.Count; i++)
+            {
+                if (array[i] < minVal) { minVal = (int)array[i]; comprassion++; }
+                else if (array[i] > maxVal) { maxVal = (int)array[i]; comprassion++; }
+            }
+            int[] counts = new int[(maxVal - minVal) + 1];
+            for (int i = 0; i < array.Count; i++) counts[(int)(array[i] - minVal)]++;
+            counts[0]--;
+            for (int i = 1; i < counts.Length; i++) counts[i] = counts[i] + counts[i - 1];
+            for (int i = array.Count - 1; i >= 0; i--){ sortedArray[counts[(int)(array[i] - minVal)]--] = (int)array[i]; swapping++; }
+            return sortedArray;
+        }
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            button2.Enabled = true ? textBox1.Text.Length != 0: button2.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Cleaning();
+                button1.Enabled = false;
+                string q = File.ReadAllText(openFileDialog1.FileName);
+                textBox1.Text = q.Replace(" ", "\r\n");
+                string[] texfile = q.Split(new string[] { "\n", " ", "\0" }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string Line in texfile)
+                {
+                    double[] temp = Line.Split(new char[] { '\n', ' ', '\r' }).Select(double.Parse).ToArray();
+                    for (int i = 0; i < temp.Length; i++)
+                    {
+                        numb.Add(temp[i]);
+                    }
+                }
+                button1.Enabled = true;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            textBox3.Clear();
+            label1.Text = "Время:   ";
+            label2.Text = "Кол-во перестановок:";
+            label3.Text = "Кол-во сравнений:";
+            label6.Text = "Время:   ";
+            label5.Text = "Кол-во перестановок:";
+            label4.Text = "Кол-во сравнений:";
+            button2.Enabled = false;
+            bool doubleDect = false;
+            for (int j = 0; j < numb.Count; j++)
+            {
+                doubleDect = (Math.Abs(numb[j] % 1) <= (double.Epsilon * 100)) ? false : true;
+                if (doubleDect) break;
+            }
+            double[] n = numb.ToArray();
+
+            var Start = DateTime.Now;
+            QuickSort(n, 0, numb.Count - 1);
+            var spendtime = DateTime.Now - Start;
+            List<string>  tempr = new List<string>();
+            for (int i = 0; i < n.Length; i++) tempr.Add(Convert.ToString(n[i]));
+            string g = String.Join("\r\n", tempr);
+            textBox2.Text = g;
+            label1.Text = "Время:   " + spendtime.TotalMilliseconds + " мс";
+            label2.Text = "Кол-во перестановок:" + swapping;
+            label3.Text = "Кол-во сравнений:" + comprassion;
+            if (doubleDect)
+            {
+                string message = "В Файле обнаруруженны вещественные числа. Сортировка с подчётом используется только для" +
+                    "натуральных чисел. Для работы сортировки следует удалить вещественные. В противном, сортировка не запустится. Удалить вещественные числа?";
+                string capricon = "Внимание!";
+                if (MessageBox.Show(message, capricon, MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes)
+                {
+                    List<double> n2 = new List<double>();
+                    for (int i = 0; i < numb.Count; i++)
+                    {
+                        if (Math.Abs(numb[i] % 1) <= Double.Epsilon * 100)
+                        {
+                            n2.Add(numb[i]);
+                        }
+                    }
+                    CountingVoid(n2);
+
+                }
+
+            }
+            else CountingVoid(numb);
+            button2.Enabled = true;
+        }
+    }
+}
